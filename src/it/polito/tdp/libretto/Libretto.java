d2be0a2efe5f4e363e27d47dcbc291c20836c479
Sdaxddx <Sdaxddx@172.21.57.162>
2018-03-13 18:03:52 +0100

    v1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,31 @@
 package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	List<Voto> voti;
+
+	public Libretto() {
+		this.voti = new ArrayList<>();
+	}
+	
+	public void add(Voto v) {
+		voti.add(v);
+	}
+	
+	public void stampa() {
+		System.out.println("Ci sono " + voti.size() + " voti\n"); 
+		for(Voto v : voti) {
+			System.out.println(v);
+		}
+	}
+	
+	public void stampaMaggioreDi(int voto) {
+		for(Voto v : voti) {
+			if(v.getVoto()> voto) 
+				System.out.println(v);
+		}
+	}
 	
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,60 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
+// POJO (plain old java object)
+
 public class Voto {
+	private String esame;
+	private int voto;
+	// private LocalDate data;
+	
+	/**
+	 * @param esame
+	 * @param voto
+	 * @param data
+	 */
+	public Voto(String esame, int voto) {
+		this.esame = esame;
+		this.voto = voto;
+		// this.data = data;
+	}
+
+	/**
+	 * @return the esame
+	 */
+	public String getEsame() {
+		return esame;
+	}
+
+	/**
+	 * @param esame the esame to set
+	 */
+	public void setEsame(String esame) {
+		this.esame = esame;
+	}
+
+	/**
+	 * @return the voto
+	 */
+	public int getVoto() {
+		return voto;
+	}
+
+	/**
+	 * @param voto the voto to set
+	 */
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	/**
+	 * @return the name and the score
+	 */
+	public String toString() {
+		return getEsame() + " " + getVoto() + "\n";
+	}
+	
+	
 	
 }
